f17a3214d9495269bd8b4f8e82e39ba1d2ad081f
unknown <gloriajimenez010@icloud.com>
2026-02-07 13:18:43 +0100

    un poco de diseño en la 2 pantalla ciudadview.kt

--- app/src/main/java/com/example/p4_ciudades_javiervictorgloria/ui/theme/Color.kt
+++ app/src/main/java/com/example/p4_ciudades_javiervictorgloria/ui/theme/Color.kt
@@ -1,11 +1,15 @@
 package com.example.p4_ciudades_javiervictorgloria.ui.theme
 
 import androidx.compose.ui.graphics.Color
-
+/*
 val Purple80 = Color(0xFF8BC34A)
 val PurpleGrey80 = Color(0xFF2196F3)
 val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Pink40 = Color(0xFF7D5260)*/
+
+val PurpleCenter = Color(0xFF9C27B0)
+val PurpleFondo = Color(0xFFF3E5F5)
+val Pink = Color(0xFFE91E63)
--- app/src/main/java/com/example/p4_ciudades_javiervictorgloria/ui/theme/Theme.kt
+++ app/src/main/java/com/example/p4_ciudades_javiervictorgloria/ui/theme/Theme.kt
@@ -9,18 +9,26 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color(0xFFD0BCFF),
+    secondary = Color(0xFFCCC2DC),
+    tertiary = Color(0xFFEFB8C8),
+    background = Color(0xFF1C1B1F),
+    surface = Color(0xFF1C1B1F)
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Color(0xFF9C27B0),
+    onPrimary = Color.White,
+    primaryContainer = Color(0xFFF3E5F5),
+    onPrimaryContainer = Color(0xFF7B1FA2),
+    secondary = Color(0xFFE91E63),
+    background = Color(0xFFF8F0FA),
+    surface = Color.White,
+    onSurface = Color(0xFF1C1B1F)
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -37,7 +45,7 @@ private val LightColorScheme = lightColorScheme(
 fun P4_Ciudades_JavierVictorGloriaTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
